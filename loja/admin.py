619f6cda5b0ccb4207fe2bfe0a267bd8d301fbf1
Caio Henrique Cruz da Silva <20231011110009@ifrn.local>
2025-04-28 14:29:51 -0300

    Models inserted in browser admin. Categoria and Produto models created.

--- loja/admin.py
+++ loja/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import * #importa nossos models
+admin.site.register(Fabricante) #adiciona a interface do admin no browser
+admin.site.register(Categoria)
+admin.site.register(Produto)
