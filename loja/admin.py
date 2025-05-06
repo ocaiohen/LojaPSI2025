9edf3b78e6dbec285a160e1782db2447452176c6
ocaiohen <caiohencontato@gmail.com>
2025-05-06 15:50:44 -0300

    ProdutoAdmin updated.

--- loja/admin.py
+++ loja/admin.py
@@ -6,6 +6,14 @@ from .models import * #importa nossos models
 class FabricanteAdmin(admin.ModelAdmin):
     date_hierarchy = "criado_em"
 
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+    fields = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
+    search_fields = ('Produto',)
+    exclude = ('msgPromocao',)
+
 admin.site.register(Fabricante, FabricanteAdmin) #adiciona a interface do admin no browser
 admin.site.register(Categoria)
-admin.site.register(Produto)
+admin.site.register(Produto, ProdutoAdmin)
